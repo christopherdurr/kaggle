1fce3ebc7b4105f215538f562bfc1d87d2b30418
Christopher Durr <cdurr@uci.edu>
2017-01-11 21:07:26 -0800

    started learning numpy

---
+++ dataquestnumpybasics.py
@@ -0,0 +1,16 @@
+import numpy
+
+world_alcohol = numpy.genfromtxt("world_alcohol.csv", delimiter = ",")
+print(type(world_alcohol))
+
+vector = numpy.array([10, 20, 30])
+matrix = numpy.array([[5, 10, 15], [20, 25, 30], [35, 40, 45]])
+
+vector_shape = vector.shape
+matrix_shape = matrix.shape
+
+world_alcohol_dtype = world_alcohol.dtype
+
+# READ THE DATA PROPERLY
+world_alcohol = numpy.genfromtxt("world_alcohol.csv", delimiter=",", dtype = "U75", skip_header = 1)
+print(world_alcohol)
